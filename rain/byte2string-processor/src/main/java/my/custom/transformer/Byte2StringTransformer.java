c82a850a0c2e5334f1262d0bda00167b208aa119
zhb <zhouhb@dtdream.com>
2016-04-14 10:42:58 +0800

    modify rain dirent structure and add byte2string-processor

---
+++ rain/byte2string-processor/src/main/java/my/custom/transformer/Byte2StringTransformer.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package my.custom.transformer;
+
+import java.util.Arrays;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Transformer;
+
+
+/**
+ * @author David Turanski
+ */
+@MessageEndpoint
+public class Byte2StringTransformer {
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Transformer(inputChannel = "input", outputChannel = "output")
+	public String transform(byte[] payload) {
+		System.out.println(Arrays.toString(payload));
+		return new String(payload);
+	}
+}
